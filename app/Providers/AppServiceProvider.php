d7ea363e324083e4db8245df71f4e74ddf6579b0
VictorDev <victordasilvafernandes@gmail.com>
2024-09-13 01:25:36 -0300

    feat: starting to configuring stripe/cashier

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Laravel\Cashier\Cashier;
 use Laravel\Sanctum\PersonalAccessToken;
 use Laravel\Sanctum\Sanctum;
 
@@ -22,5 +23,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Sanctum::usePersonalAccessTokenModel(PersonalAccessToken::class);
+        Cashier::calculateTaxes();
     }
 }
